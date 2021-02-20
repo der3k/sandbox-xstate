dfd234af361967ad74b1f019a937bfda083ba4d0
silesnet <>
2021-02-20 21:19:03 +0100

    Create simple FSM skeleton

--- src/test/app.spec.ts
+++ src/test/app.spec.ts
@@ -1,7 +1,16 @@
 import { expect } from "chai"
+import { interpret } from 'xstate';
+import { toggleMachine } from "../lib/pairing-fsm";
 
 describe('Application', () => {
   it('should work', () => {
+
+    const toggleService = interpret(toggleMachine)
+      .onTransition((state) => console.log(state.value))
+      .start();
+    toggleService.send('TOGGLE');
+    toggleService.send('TOGGLE');
+
     expect(true).to.be.true;
   })
 })
